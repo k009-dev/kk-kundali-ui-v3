2597c12d6c68c56a8f44b072be09d8a7751e9b76
k009-dev <0x0x0x0x0x009@gmail.com>
2025-09-24 16:10:29 +0530

    gv

---
+++ src/app/components/HouseDivs.js
@@ -0,0 +1,36 @@
+export default function HouseDivs() {
+  return (
+    <div style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      zIndex: 15
+    }}>
+      {/* House 1 */}
+      <div style={{
+        position: 'absolute',
+        top: '20%',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        width: '8%',
+        height: '23%',
+        border: '1px solid gold'
+      }}>
+      </div>
+      
+      {/* House 7 */}
+      <div style={{
+        position: 'absolute',
+        bottom: '20%',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        width: '8%',
+        height: '23%',
+        border: '1px solid gold'
+      }}>
+      </div>
+    </div>
+  );
+}
--- src/app/components/KundaliMain.js
+++ src/app/components/KundaliMain.js
@@ -1,4 +1,5 @@
 import HouseNumbers from './HouseNumbers';
+import HouseDivs from './HouseDivs';
 
 export default function KundaliMain() {
   return (
@@ -13,6 +14,7 @@ export default function KundaliMain() {
         position: 'relative'
       }}
     >
+      <HouseDivs />
       <HouseNumbers />
     </div>
   );
