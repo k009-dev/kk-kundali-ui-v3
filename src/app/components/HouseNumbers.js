0f46949720a777f535f8692e7c54a62c65a3b35d
k009-dev <0x0x0x0x0x009@gmail.com>
2025-09-24 15:36:30 +0530

    gfcg

--- src/app/components/HouseNumbers.js
+++ src/app/components/HouseNumbers.js
@@ -15,153 +15,153 @@ export default function HouseNumbers() {
         left: '50%',
         transform: 'translateX(-50%)',
         color: 'gold',
-        fontSize: '16px',
+        fontSize: '14px',
         fontWeight: 'bold'
       }}>
         1
       </div>
       
-      {/* House 2 */}
+      {/* 2 */}
       <div style={{
         position: 'absolute',
         top: '24%',
         left: '68%',
         transform: 'translateX(-50%)',
         color: 'gold',
-        fontSize: '16px',
+        fontSize: '14px',
         fontWeight: 'bold'
       }}>
         2
       </div>
       
-      {/* House 4 */}
+      {/* 3 */}
       <div style={{
         position: 'absolute',
-        top: '48%',
-        left: '85%',
+        top: '38%',
+        left: '78%',
         transform: 'translateX(-50%)',
         color: 'gold',
-        fontSize: '16px',
+        fontSize: '14px',
+        fontWeight: 'bold'
+      }}>
+        3
+      </div>
+      
+      {/* 4 */}
+      <div style={{
+        position: 'absolute',
+        top: '48.5%',
+        left: '84%',
+        transform: 'translateX(-50%)',
+        color: 'gold',
+        fontSize: '14px',
         fontWeight: 'bold'
       }}>
         4
       </div>
       
-      {/* House 6 */}
+      {/* 5 */}
+      <div style={{
+        position: 'absolute',
+        top: '58%',
+        left: '78%',
+        transform: 'translateX(-50%)',
+        color: 'gold',
+        fontSize: '14px',
+        fontWeight: 'bold'
+      }}>
+        5
+      </div>
+      
+      {/* 6 */}
       <div style={{
         position: 'absolute',
         top: '72%',
         left: '68%',
         transform: 'translateX(-50%)',
         color: 'gold',
-        fontSize: '16px',
+        fontSize: '14px',
         fontWeight: 'bold'
       }}>
         6
       </div>
       
-      {/* House 7 */}
+      {/* 7 */}
       <div style={{
         position: 'absolute',
         top: '81%',
         left: '50%',
         transform: 'translateX(-50%)',
         color: 'gold',
-        fontSize: '16px',
+        fontSize: '14px',
         fontWeight: 'bold'
       }}>
         7
       </div>
       
-      {/* House 8 */}
+      {/* 8 */}
       <div style={{
         position: 'absolute',
         top: '72%',
         left: '32%',
         transform: 'translateX(-50%)',
         color: 'gold',
-        fontSize: '16px',
+        fontSize: '14px',
         fontWeight: 'bold'
       }}>
         8
       </div>
       
-      {/* House 10 */}
+      {/* 9 */}
       <div style={{
         position: 'absolute',
-        top: '48%',
-        left: '15%',
+        top: '58%',
+        left: '22%',
         transform: 'translateX(-50%)',
         color: 'gold',
-        fontSize: '16px',
+        fontSize: '14px',
+        fontWeight: 'bold'
+      }}>
+        9
+      </div>
+      
+      {/* 10 */}
+      <div style={{
+        position: 'absolute',
+        top: '48.5%',
+        left: '16%',
+        transform: 'translateX(-50%)',
+        color: 'gold',
+        fontSize: '14px',
         fontWeight: 'bold'
       }}>
         10
       </div>
       
-      {/* House 12 */}
+      {/* 11 */}
+      <div style={{
+        position: 'absolute',
+        top: '38%',
+        left: '22%',
+        transform: 'translateX(-50%)',
+        color: 'gold',
+        fontSize: '14px',
+        fontWeight: 'bold'
+      }}>
+        11
+      </div>
+      
+      {/* 12 */}
       <div style={{
         position: 'absolute',
         top: '24%',
         left: '31%',
         transform: 'translateX(-50%)',
         color: 'gold',
-        fontSize: '16px',
+        fontSize: '14px',
         fontWeight: 'bold'
       }}>
         12
-      </div>
-      
-      {/* House 11 */}
-      <div style={{
-        position: 'absolute',
-        top: '38%',
-        left: '22%',
-        transform: 'translateX(-50%)',
-        color: 'gold',
-        fontSize: '16px',
-        fontWeight: 'bold'
-      }}>
-        11
-      </div>
-      
-      {/* House 3 */}
-      <div style={{
-        position: 'absolute',
-        top: '38%',
-        left: '78%',
-        transform: 'translateX(-50%)',
-        color: 'gold',
-        fontSize: '16px',
-        fontWeight: 'bold'
-      }}>
-        3
-      </div>
-      
-      {/* House 5 */}
-      <div style={{
-        position: 'absolute',
-        top: '58%',
-        left: '78%',
-        transform: 'translateX(-50%)',
-        color: 'gold',
-        fontSize: '16px',
-        fontWeight: 'bold'
-      }}>
-        5
-      </div>
-      
-      {/* House 9 */}
-      <div style={{
-        position: 'absolute',
-        top: '58%',
-        left: '22%',
-        transform: 'translateX(-50%)',
-        color: 'gold',
-        fontSize: '16px',
-        fontWeight: 'bold'
-      }}>
-        9
       </div>
     </div>
   );
